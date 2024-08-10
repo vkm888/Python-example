864b7315a8089862b72418cabf39dd92366a201d
vkm888 <vkm@ukr.net>
2024-08-11 02:09:52 +0300

    авто клік мишки

--- auto_click.py
+++ auto_click.py
@@ -1,9 +1,14 @@
-# код виконує авто клік мишки для перевірки наступний сайт
-# https://clickspeedtest.com/10-seconds.html
+# код виконує авто клік мишки
+# '`' для start/finish
+# для перевірки відкривається наступний сайт  https://clickspeedtest.com/10-seconds.html
 
 import keyboard
 import mouse
 import time
+import webbrowser
+
+# Відкрити нову вкладку в браузері з вказаним сайтом
+webbrowser.open_new_tab("https://clickspeedtest.com/10-seconds.html")
 
 def change():
     global work
