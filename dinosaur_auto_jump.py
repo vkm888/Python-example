87cd9df77edecdd5c75a7921771bf9868a57eab2
vkm888 <vkm@ukr.net>
2024-08-11 12:04:24 +0300

    Final auto jump Dinosaur T-Rex

--- dinosaur_auto_jump.py
+++ dinosaur_auto_jump.py
@@ -13,6 +13,12 @@ import numpy as np
 # import winsound
 import webbrowser
 
+# start/finish при натисканні " ` "
+work = False
+def change():
+    global work
+    work = not work
+keyboard.add_hotkey('`', change)
 
 # Координати ділянки екрану
 box = (733, 383, 800, 420)
@@ -24,23 +30,33 @@ root.title("Скріншот області екрану")
 # Встановити розташування вікна (наприклад, 300x200 розмір і розташування вікна на 100 пікселів зліва і 100 пікселів зверху)
 root.geometry("300x200+1100+800")
 
-# Створити віджет Label
-label = tk.Label(root)
-label.pack()
+# Створити віджет Label для відображення значення
+color_label = tk.Label(root, text="Сумарне значення кольору: ")
+color_label.pack()
 
-# Функція оновлення зображення для вікна tkinter
+# Створити віджет Label для відображення зображення
+image_label = tk.Label(root)
+image_label.pack()
+
+# Функція для оновлення скріншоту і відображення значення
 def update_screenshot():
     screenshot = ImageGrab.grab(box)
+    gray_image = ImageOps.grayscale(screenshot)
+    a = np.array(gray_image.getcolors())
+
+    # Оновити текст у Label для значення кольору
+    color_sum = a.sum()
+    color_label.config(text=f"Сумарне значення кольору: {color_sum}")
+
+    # Оновити зображення у Label для відображення скріншоту
     tk_image = ImageTk.PhotoImage(screenshot)
-    label.config( image = tk_image )
-    label.image = tk_image  # Зберегти посилання на зображення, щоб воно не видалилось збирачем сміття
+    image_label.config(image=tk_image)
+    image_label.image = tk_image  # Зберегти посилання на зображення
+
     root.update_idletasks()
     root.update()
 
-def change():
-    global work
-    work = not work
-
+# Функція "Натискання пробілу"
 def press_space():
     print("Перешкода виявлена! Натискання пробілу.")
     pyautogui.keyDown('space')
@@ -54,9 +70,10 @@ def press_space():
     # time.sleep(0.05)
     # pyautogui.keyUp('down')
 
+# Функція виявлення перешкод через зміну значень пікселів
 def detect_obstacle():
     # Координати прямокутника, де ми шукаємо перешкоди
-    box = (733, 383, 800, 420) #(280, 400, 400, 450)
+    # box = (733, 383, 800, 420)
     image = ImageGrab.grab(box)
     gray_image = ImageOps.grayscale(image)
     a = np.array(gray_image.getcolors())
@@ -65,18 +82,15 @@ def detect_obstacle():
     print("Сумарне значення кольору: ", a.sum())
 
     # Якщо перешкода наближається, значення пікселів змінюється
-    if a.sum() > 2721: # 1447  3202
+    if a.sum() > 2721:
         return True
     return False
 
-# start/finish при натисканні " ` "
-work = False
-keyboard.add_hotkey('`', change)
-
-# Відкрити нову вкладку в браузері з вказаним сайтом
+# Відкриття нової вкладки в браузері з вказаним сайтом
 webbrowser.open_new_tab("https://dinorunner.com")
 
-time.sleep(2)  # Затримка для переходу до гри
+# Затримка для переходу до гри
+# time.sleep(2)
 
 while True:
     if work:
